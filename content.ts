1005ab6a4483c45c7df494b34c3af212212ba20a
Henri <98414850+ProHen9@users.noreply.github.com>
2023-10-11 17:21:08 +0200

    Extension fertig gestellt.

--- content.ts
+++ content.ts
@@ -1,13 +1,10 @@
 export {}
 if (window.location.host == "www.youtube.com") {
   // Element mit der Klasse "ytd-enforcement-message-view-model" auswählen
-  var elementToRemove = document.querySelector(".ytd-popup-container")
-
-  // Überprüfen, ob das Element gefunden wurde, bevor es entfernt wird
-  if (elementToRemove) {
-    // Element entfernen
-    elementToRemove.remove()
-  }
+  console.log("wird erledigt")
+  var elements = document.querySelector("ytd-popup-container")
+  console.log(elements)
+  elements.remove()
 } else {
   console.log(window.location.host)
 }
