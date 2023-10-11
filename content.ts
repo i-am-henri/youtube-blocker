bfa73eb02c8c9e1e695a39cdf2bd383ca0f6389d
Henri <98414850+ProHen9@users.noreply.github.com>
2023-10-11 17:21:42 +0200

    console.logs entfernt

--- content.ts
+++ content.ts
@@ -1,10 +1,5 @@
 export {}
 if (window.location.host == "www.youtube.com") {
-  // Element mit der Klasse "ytd-enforcement-message-view-model" auswählen
-  console.log("wird erledigt")
   var elements = document.querySelector("ytd-popup-container")
-  console.log(elements)
   elements.remove()
-} else {
-  console.log(window.location.host)
 }
